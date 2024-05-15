181adc159067c8044802002997405b6390fa8727
Sreedatta Vajinapally <111418043+sreedatta-v@users.noreply.github.com>
2024-05-15 22:43:33 +0530

    Add files via upload

---
+++ sorting_numpy.py
@@ -0,0 +1,6 @@
+# Sorting an element is simple with np.sort(). You can specify the axis, kind, and order when you call the function.
+
+import numpy as np
+arr = np.sort([2, 1, 5, 3, 7, 4, 6, 8])
+print(arr)
+
